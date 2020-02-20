a64ffdc0214ef12edcd0af1baa31baf1d74723ec
Ngethe <ciru930@gmail.com>
2020-02-20 10:58:22 +0300

    initialized the sql alchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # ....
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
