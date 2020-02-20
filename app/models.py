7e417c8592eea3f36445e00cc1f2ebef7e2ac4a2
Ngethe <ciru930@gmail.com>
2020-02-20 12:55:52 +0300

    added blue prints in the init.py file

--- app/models.py
+++ app/models.py
@@ -2,10 +2,22 @@ from . import db
 
 #...
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+# class User(db.Model):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer,primary_key = True)
+#     username = db.Column(db.String(255))
+#     role_id = db.relationship('Role',backref = 'role',lazy="dynamic")
 
-    def __repr__(self):
-        return f'User {self.username}'
+#     def __repr__(self):
+#         return f'User {self.username}'
+
+# class Role(db.Model):
+#     __tablename__ = 'roles'
+
+#     id = db.Column(db.Integer,primary_key = True)
+#     name = db.Column(db.String(255))
+#     user_role = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
+#     def __repr__(self):
+#         return f'User {self.name}'        
