24cd8b5fb319229e7450faad11a616d515876677
Ngethe <ciru930@gmail.com>
2020-02-20 13:11:22 +0300

    addedd the blue prints in init.py file in main folder inside the app

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from config import config_options
+
 # from config import Config
 app = Flask(__name__)
 db = SQLAlchemy(app)
